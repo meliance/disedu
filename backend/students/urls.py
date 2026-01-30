69147e58a06951cbf7b936e4f3ce981642fda900
meliance <melakmu66@gmail.com>
2026-01-30 17:21:19 +0300

    fix urls

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -8,6 +8,7 @@ from .views import (
 app_name = "students"
 
 urlpatterns = [
+    path("dashboard/", student_dashboard, name="student_dashboard"),
     path("me/", StudentProfileView.as_view(), name="my-profile"),
     path("", StudentListView.as_view(), name="student-list"),
     path("<int:pk>/approve/", ApproveStudentView.as_view(), name="approve-student"),
